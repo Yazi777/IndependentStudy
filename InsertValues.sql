2bff69ae851d6702b0cfe43ed309747af667c8ba
Yazi777 <dacostay@simmons.edu>
2016-12-12 18:02:50 -0500

    Delete InsertValues.sql

--- InsertValues.sql
+++
@@ -1,7 +0,0 @@
-USE xxxxxxx;
-
-#name(varchar), serving size(varchar), carbs(int), protein(int), vitamins(y/n)
-
-INSERT INTO Diabetes (Name, ServingSize, Carbs, Protein)
-VALUES
-('Almonds', '15 count', 3, 4);
